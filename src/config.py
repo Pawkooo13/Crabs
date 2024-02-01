b18ba12586e789441ce46470c7e0c5b8f8918760
Martyna <martyna.szawlowska@gmail.com>
2024-02-01 14:19:14 +0100

    Added metrics_path in config.py

--- src/config.py
+++ src/config.py
@@ -4,3 +4,6 @@ DATA_DIR = os.path.join(os.getcwd(), 'data')
 DATA_PATH = os.path.join(DATA_DIR, 'train.csv')
 
 MODELS_DIR = os.path.join(os.getcwd(), 'models')
+MODELS_PATH = os.path.join(MODELS_DIR, 'ANN.keras')
+TEST_PATH = os.path.join(DATA_DIR, 'test.csv')
+METRICS_PATH = os.path.join(MODELS_DIR, 'metrics.json')
